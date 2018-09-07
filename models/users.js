be9a5cfe1a77ccea18fac2ac93e0aa0286cefa8a
Adarsh Rao <raoadarsh123@gmail.com>
2018-09-07 10:32:24 -0400

    hello

--- models/users.js
+++ models/users.js
@@ -1,6 +1,17 @@
-var kyle = "smart";
-console.log(kyle);
+var express = require('express');
+var app = express();
 
-var aneesh = 'gay';
-console.log(aneesh);
+var schema = mongoose.schema;
+
+var userInfoSchema = new Schema( {
+	userName: {type: String, required: true},
+	location: {type: String, required: true}
+});
+
+var user = mongoose.model("User", userInfoSchema);
+
+var newUser = new User ({
+	userName: req.body.userName,
+	location: req.body.location
+});
 
