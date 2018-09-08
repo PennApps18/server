79a7b91164d5e12559c49b13c1d00f2dffce75b3
Ivan Chau <ivanmchau@gmail.com>
2018-09-07 20:58:44 -0400

    routes spacing

--- routes/index.js
+++ routes/index.js
@@ -10,45 +10,44 @@ router.get('/', function(req, res, next) {
 });
 /*
  * @params username of user
- * @params new set of coordinates to update
- * 
+ * @params new set of coordinates to update 
  */
- router.post('/updateLocation', function(req, res, next){
- 	User.findOne({ username : req.body.username }, function (err, user){
- 		if (user) {
- 			user.coordinates = req.body.coordinates;
- 			user.save();
+router.post('/updateLocation', function(req, res, next){
+	User.findOne({ username : req.body.username }, function (err, user){
+		if (user) {
+			user.coordinates = req.body.coordinates;
+			user.save();
 
- 			res.writeHead(200, {"Content-Type": "application/json"});
- 			res.end(JSON.stringify(user));
- 		} else {
- 			return res.status(400).end('User does not exist')
- 		}
- 	});
- });
+			res.writeHead(200, {"Content-Type": "application/json"});
+			res.end(JSON.stringify(user));
+		} else {
+			return res.status(400).end('User does not exist')
+		}
+	});
+});
 
- router.get('/users', function (req, res, next){
- 	User.find({}, (err, users) => {
- 		if (err) {
- 			res.type('html').status(500);
- 			res.send("Error" + err);
- 		} else {
- 			console.log(users);
- 			res.send(JSON.stringify(users));
- 		}
- 	});
- });
+router.get('/users', function (req, res, next){
+	User.find({}, (err, users) => {
+		if (err) {
+			res.type('html').status(500);
+			res.send("Error" + err);
+		} else {
+			console.log(users);
+			res.send(JSON.stringify(users));
+		}
+	});
+});
 
- router.post('/range', function(req, res, next){
- 	var returnStatement  = [];
- 	Incident.forEach((incident) => {
- 		if ((incident.coordinates.lat - user.coordinates.lat >= 0.5) && (incident.coordinates.long - user.coordinates.long >= 0.5));
- 		returnStatment.push(incident);
- 	};
- 	res.send(JSON.stringify(incident);
- });
+router.post('/range', function(req, res, next){
+	var returnStatement  = [];
+	Incident.forEach((incident) => {
+		if ((incident.coordinates.lat - user.coordinates.lat >= 0.5) && (incident.coordinates.long - user.coordinates.long >= 0.5));
+		returnStatment.push(incident);
+	});
+	res.send(JSON.stringify(incident);
+});
 
 
 
- 	module.exports = router;
+module.exports = router;
 
