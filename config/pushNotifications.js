35d9cfd679f3ecb63ee1b5d0817e74039fc13bfe
Adarsh Rao <raoadarsh123@gmail.com>
2018-09-07 22:22:48 -0400

    preventing crash in notif

--- config/pushNotifications.js
+++ config/pushNotifications.js
@@ -1,1 +1,29 @@
-pf
+import { PushNotificationIOS } from 'react-native';
+var pushNotification = require('react-native-push-notification');
+
+pushNotification.configure({
+
+	onSubscriptions = function(token){
+		console.log('TOKEN:', token);
+	},
+
+	onNotification = function(notification){
+		console.log('NOTIFICATION:', notification);
+		notification.finish(PushNotificationIOS.FetchResult.NoData);
+	},
+
+	permissions: {
+		alert: true,
+		badge: true,
+		sound: true,
+	},
+
+	popInitialNotification: false,
+	requestPermissions: true,
+});
+
+
+
+
+
+
