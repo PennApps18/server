67f6c3ee31e7eb6e8f097c338cf472e0f47d3fef
Adarsh Rao <raoadarsh123@gmail.com>
2018-09-08 10:33:25 -0400

    savan

--- routes/index.js
+++ routes/index.js
@@ -14,73 +14,86 @@ module.exports = function(io){
 	 * @params username of user
 	 * @params new set of coordinates to update 
 	 */
-	router.post('/updateLocation', function(req, res, next){
-		User.findOne({ username : req.body.username }, function (err, user){
-			if (user) {
-				user.coordinates = req.body.coordinates;
-				user.save();
+	 router.post('/updateLocation', function(req, res, next){
+	 	User.findOne({ username : req.body.username }, function (err, user){
+	 		if (user) {
+	 			user.coordinates = req.body.coordinates;
+	 			user.save();
 
-				res.writeHead(200, {"Content-Type": "application/json"});
-				res.end(JSON.stringify(user));
-			} else {
-				return res.status(400).end('User does not exist')
-			}
-		});
-	});
+	 			res.writeHead(200, {"Content-Type": "application/json"});
+	 			res.end(JSON.stringify(user));
+	 		} else {
+	 			return res.status(400).end('User does not exist')
+	 		}
+	 	});
+	 });
 
-	router.get('/users', function (req, res, next){
-		User.find({}, (err, users) => {
-			if (err) {
-				res.type('html').status(500);
-				res.send("Error" + err);
-			} else {
-				console.log(users);
-				res.send(JSON.stringify(users));
-			}
-		});
-	});
+	 router.get('/users', function (req, res, next){
+	 	User.find({}, (err, users) => {
+	 		if (err) {
+	 			res.type('html').status(500);
+	 			res.send("Error" + err);
+	 		} else {
+	 			console.log(users);
+	 			res.send(JSON.stringify(users));
+	 		}
+	 	});
+	 });
 
-	router.post('/range', function(req, res, next){
-		var returnStatement  = [];
-		Incident.find({}, function(err, incidents){
-			incidents.forEach((incident) => {
-				if(!incident.resolved){
-					if (Math.abs(incident.coordinates.lat - req.body.coordinates.lat) <= 0.5 && Math.abs(incident.coordinates.long - req.body.coordinates.long) <= 0.5){
-						returnStatement.push(incident);
-					}
-				}
-			});
-			res.send(JSON.stringify(returnStatement));
-		});
-		
-	});
+	 router.post('/range', function(req, res, next){
+	 	var returnStatement  = [];
+	 	var numFood;
+ 		var numInjury;
+ 		var numOther;
+	 	Incident.find({}, function(err, incidents){
+	 		incidents.forEach((incident) => {
+	 			if(!incident.resolved){
+	 				if (Math.abs(incident.coordinates.lat - req.body.coordinates.lat) <= 0.5 && Math.abs(incident.coordinates.long - req.body.coordinates.long) <= 0.5){
+	 					returnStatement.push(incident);
+	 				}
+	 			}
+		 		
+		 		for(var property in incident){
+		 			if (incident.food === true){
+		 				numFood += 1;
+		 			} else if (incident.injury === true){
+		 				numInjury += 1;
+		 			} else if (incident.other === true){
+		 				numOther += 1;
+		 			}
+		 		}
+	 		});
+	 		res.send(JSON.stringify({incidents: returnStatement, report: {food: numFood, injury: numInjury, other: numOther}}));
+	 	});
 
-router.post('/storedUsers', function(req, res, next){
-	var dbUser = new Incident();
-		dbUser.user = req.body.user;
-		dbUser.description = req.body.description;
-		dbUser.coordinates = req.body.coordinates;
-		dbUser.currentPriority = req.body.currentPriority;
-		dbUser.resolved = false;
-		dbUser.image = req.body.image;
 
-		dbUser.save();
-		
-	io.emit("incidentUpdate", dbUser);
+	 });
 
-	res.status(200).end("end");
-});
+	 router.post('/storedUsers', function(req, res, next){
+	 	var dbUser = new Incident();
+	 	dbUser.user = req.body.user;
+	 	dbUser.description = req.body.description;
+	 	dbUser.coordinates = req.body.coordinates;
+	 	dbUser.currentPriority = req.body.currentPriority;
+	 	dbUser.resolved = false;
+	 	dbUser.image = req.body.image;
 
-router.post('/resolveIncident', function (req, res, next){
-	const id = req.body.incidentId;
-	Incident.findById(id, function (err, incident){
-		incident.resolved = true;
-		incident.save();
-		res.status(200).end("Resolve Succ");
-	});
-});
+	 	dbUser.save();
 
-return router;
+	 	io.emit("incidentUpdate", dbUser);
+	 	res.status(200).end("end");
+	 });
 
-}
+	 router.post('/resolveIncident', function (req, res, next){
+	 	const id = req.body.incidentId;
+	 	Incident.findById(id, function (err, incident){
+	 		incident.resolved = true;
+	 		incident.save();
+	 		res.status(200).end("Resolve Succ");
+	 	});
+	 });
+
+	 return router;
+
+	}
 
