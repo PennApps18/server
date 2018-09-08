969569eb07c3c75e375373d72b859709062d4a3f
Adarsh Rao <raoadarsh123@gmail.com>
2018-09-07 20:51:42 -0400

    incidents

--- routes/index.js
+++ routes/index.js
@@ -1,41 +1,54 @@
 var express = require('express');
 var User = require('../models/user');
+var Incident = require('../models/incident');
 var router = express.Router();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
 });
 /*
  * @params username of user
  * @params new set of coordinates to update
  * 
  */
-router.post('/updateLocation', function(req, res, next){
-	User.findOne({ username : req.body.username }, function (err, user){
-		if (user) {
-			user.coordinates = req.body.coordinates;
-			user.save();
+ router.post('/updateLocation', function(req, res, next){
+ 	User.findOne({ username : req.body.username }, function (err, user){
+ 		if (user) {
+ 			user.coordinates = req.body.coordinates;
+ 			user.save();
 
-			res.writeHead(200, {"Content-Type": "application/json"});
-			res.end(JSON.stringiy(user));
-		} else {
-			return res.status(400).end('User does not exist')
-		}
-	});
-});
+ 			res.writeHead(200, {"Content-Type": "application/json"});
+ 			res.end(JSON.stringiy(user));
+ 		} else {
+ 			return res.status(400).end('User does not exist')
+ 		}
+ 	});
+ });
 
-router.get('/users', function (req, res, next){
-	User.find({}, (err, users) => {
-		if (err) {
-			res.type('html').status(500);
-			res.send("Error" + err);
-		} else {
-			console.log(users);
-			res.send(JSON.stringify(users));
-		}
-	});
-});
+ router.get('/users', function (req, res, next){
+ 	User.find({}, (err, users) => {
+ 		if (err) {
+ 			res.type('html').status(500);
+ 			res.send("Error" + err);
+ 		} else {
+ 			console.log(users);
+ 			res.send(JSON.stringify(users));
+ 		}
+ 	});
+ });
 
-module.exports = router;
+ router.post('/range', function(req, res, next){
+ 	var returnStatement  = [];
+ 	Incident.forEach((incident) => {
+ 		if ((incident.coordinates.lat - user.coordinates.lat >= 0.5) && (incident.coordinates.long - user.coordinates.long >= 0.5));
+ 		returnStatment.push(incident);
+ 	};
+ 	res.send(JSON.stringify(incident);
+ });
+
+
+
+ 	module.exports = router;
 
